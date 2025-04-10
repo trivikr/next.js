4da623d31b9cdfab48957bddf179cc1e45c39019
Janka Uryga <lolzatu2@gmail.com>
2025-04-10 02:55:15 +0200

    limit elementByCss to 5s instead of the default 60s

--- test/lib/browsers/playwright.ts
+++ test/lib/browsers/playwright.ts
@@ -472,7 +472,7 @@ export class Playwright extends BrowserInterface {
 
   elementByCss(selector: string) {
     this.assertReady()
-    return this.waitForElementByCss(selector)
+    return this.waitForElementByCss(selector, 5_000)
   }
 
   elementById(sel) {
